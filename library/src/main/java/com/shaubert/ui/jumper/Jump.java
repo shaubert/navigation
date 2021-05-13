791339802796b2a1eca435e82182e3e178c05578
alex <alex@wirexapp.com>
2021-05-13 17:28:59 +0200

    Fix Jump interface Inc version

--- library/src/main/java/com/shaubert/ui/jumper/Jump.java
+++ library/src/main/java/com/shaubert/ui/jumper/Jump.java
@@ -1,8 +1,23 @@
 package com.shaubert.ui.jumper;
 
 import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.core.app.ActivityOptionsCompat;
 
 public interface Jump<ARGS extends Args, CONFIG extends Config> extends BlindJump<ARGS, CONFIG> {
+
+    @Override
+    Jump<ARGS, CONFIG> withExtras(Bundle extras);
+
+    @Override
+    Jump<ARGS, CONFIG> withArgs(ARGS args);
+
+    @Override
+    Jump<ARGS, CONFIG> withActivityOptions(ActivityOptionsCompat activityOptions);
+
+    @Override
+    Jump<ARGS, CONFIG> withConfig(CONFIG config);
 
     Intent getIntent();
 
